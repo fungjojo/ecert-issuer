9819b2e4d5b669ef59d3f749576bced95ee1d114
Joanne <>
2022-07-17 23:34:51 +0800

    add env var

--- cert_issuer/blockchain_handlers/ethereum/transaction_handlers.py
+++ cert_issuer/blockchain_handlers/ethereum/transaction_handlers.py
@@ -1,4 +1,5 @@
 import logging
+import os 
 
 from pycoin.serialize import b2h
 
@@ -66,7 +67,9 @@ class EthereumTransactionHandler(TransactionHandler):
     def create_transaction(self, blockchain_bytes):
         if self.balance:
             # it is assumed here that the address has sufficient funds, as the ensure_balance has just been checked
-            nonce = 11
+            env_nonce = os.environ.get('ENV_NONCE')
+            logging.info('???? ETH env_nonce=%s', env_nonce)
+            nonce = env_nonce
             # nonce = self.connector.get_address_nonce(self.issuing_address)
             logging.info('???? ETH nonce=%s', nonce)
             # Transactions in the first iteration will be send to burn address
