a762beb997f1343c414e4a89ad11cd2fabee2b15
Joanne <>
2022-07-24 17:03:38 +0800

    add copy signed cert script

--- issue-cert.sh
+++ issue-cert.sh
@@ -12,5 +12,8 @@ echo "[issue-cert] start cert issuing"
 
 cd cert-issuer && cert-issuer -c conf.ini --verification_method "0xEd76Cba060A1c7210c7e10EbE562b4966B9f45A1" > log.txt
 
+echo "[issue-cert] copy signed cert to cert-issuer"
+cp /etc/cert-issuer/data/blockchain_certificates/* cert-issuer/signed
+
 echo "[issue-cert] print log"
 cat log.txt
